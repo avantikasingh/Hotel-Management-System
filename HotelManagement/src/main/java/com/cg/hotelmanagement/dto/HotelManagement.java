691a8f0a5f085e3e2fbceb8e9576cfc5d66de50b
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2019-09-07 16:02:04 +0530

    Add files via upload

---
+++ HotelManagement/src/main/java/com/cg/hotelmanagement/dto/HotelManagement.java
@@ -0,0 +1,15 @@
+package com.cg.hotelmanagement.dto;
+
+import java.util.List;
+
+
+public class HotelManagement {
+	private List<?> customerList;
+	private List<?> hotelList;
+	private List<?> adminList;
+	private List<?> bookingList;
+	
+	
+	
+
+}
