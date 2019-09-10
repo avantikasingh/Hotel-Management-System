771ceb2095a05addcb9ebc715cadcdd39b489bfc
Birju7 <classthaparu@gmail.com>
2019-09-10 09:59:37 +0530

    Exception

--- HotelManagement/src/main/java/com/cg/hotelmanagement/exception/HotelManagementException.java
+++ HotelManagement/src/main/java/com/cg/hotelmanagement/exception/HotelManagementException.java
@@ -2,7 +2,7 @@ package com.cg.hotelmanagement.exception;
 
 public class HotelManagementException extends Exception{
 	public HotelManagementException(String message) {
-		System.out.println(message);
+		System.out.println(message); 
 		
 	}
 
