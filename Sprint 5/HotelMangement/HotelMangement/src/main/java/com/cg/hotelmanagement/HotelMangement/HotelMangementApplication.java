289668b02c15b12ddc7101827d0857631c34a7bd
Birju <classthaparu@gmail.com>
2019-10-05 09:53:29 +0530

    Boot Project

---
+++ Sprint 5/HotelMangement/HotelMangement/src/main/java/com/cg/hotelmanagement/HotelMangement/HotelMangementApplication.java
@@ -0,0 +1,13 @@
+package com.cg.hotelmanagement.HotelMangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMangementApplication.class, args);
+	}
+
+}
