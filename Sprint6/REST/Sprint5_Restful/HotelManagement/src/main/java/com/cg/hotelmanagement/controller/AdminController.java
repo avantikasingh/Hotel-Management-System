bb8dd4870e85ea715ede19bc6c75a3cef96c5d54
Admin <Admin@DESKTOP-44JBV6N>
2019-10-20 23:00:53 -0700

    City page added and show components with html

--- Sprint6/REST/Sprint5_Restful/HotelManagement/src/main/java/com/cg/hotelmanagement/controller/AdminController.java
+++ Sprint6/REST/Sprint5_Restful/HotelManagement/src/main/java/com/cg/hotelmanagement/controller/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ import com.cg.hotelmanagement.service.ICustomerService;
 
 @RestController
 @RequestMapping("/admin")
+@CrossOrigin(origins ="http://localhost:4200")
 public class AdminController {
 
 	@Autowired
--- Sprint6/REST/Sprint5_Restful/HotelManagement/src/main/java/com/cg/hotelmanagement/controller/CustomerController.java
+++ Sprint6/REST/Sprint5_Restful/HotelManagement/src/main/java/com/cg/hotelmanagement/controller/CustomerController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +29,7 @@ import com.cg.hotelmanagement.service.Validate;
 
 @RestController
 @RequestMapping("/customer")
+@CrossOrigin(origins ="http://localhost:4200")
 public class CustomerController {
 
 	@Autowired
